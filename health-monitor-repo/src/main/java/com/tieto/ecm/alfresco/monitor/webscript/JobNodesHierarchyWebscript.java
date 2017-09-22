b0787d7c6dff27b574ba73e5edd8f55131da9f0a
Vitezslav Sliz <vitezslav.sliz@tieto.com>
2017-09-22 20:50:42 +0200

    added web script

---
+++ health-monitor-repo/src/main/java/com/tieto/ecm/alfresco/monitor/webscript/JobNodesHierarchyWebscript.java
@@ -0,0 +1,44 @@
+package com.tieto.ecm.alfresco.monitor.webscript;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.alfresco.repo.model.Repository;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.extensions.webscripts.Cache;
+import org.springframework.extensions.webscripts.DeclarativeWebScript;
+import org.springframework.extensions.webscripts.Status;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+
+import com.tieto.ecm.alfresco.monitor.service.MonitorJobService;
+import com.tieto.ecm.alfresco.monitor.service.job.NodesHierarchyJob;
+
+public class JobNodesHierarchyWebscript extends DeclarativeWebScript{
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(JobNodesHierarchyWebscript.class);
+	
+	private NodesHierarchyJob job;
+	private MonitorJobService monitorService;
+	private Repository repositoryHelper;
+	
+	public void setJob(NodesHierarchyJob job) {
+		this.job = job;
+	}
+
+	public void setMonitorService(MonitorJobService monitorService) {
+		this.monitorService = monitorService;
+	}
+
+	@Override
+	protected Map<String, Object> executeImpl(WebScriptRequest req, Status status, Cache cache) {
+		
+		final NodeRef jobNode = job.createNodesHierarchyJob(repositoryHelper.getCompanyHome());
+		monitorService.runMonitorOperation(jobNode);
+		
+		final Map<String, Object> model = new HashMap<>();
+		model.put("jobNode", jobNode.toString());
+		return super.executeImpl(req, status, cache);
+	}
+}
---
+++ health-monitor-repo/src/main/resources/alfresco/extension/templates/webscripts/com/tieto/ecm/alfresco/monitor/job/job_nodesHierarchy.get.json.ftl
@@ -0,0 +1,5 @@
+<#escape x as jsonUtils.encodeJSONString(x)>
+{       
+	"monitorJob": "${jobNode?string}",
+}
+</#escape>
