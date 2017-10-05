271d9bce49219130521419b7cbfe6b8148deaa62
Ludvik Hobza <ludvik.hobza@tieto.com>
2017-10-05 20:38:37 +0200

    Added more demo data

--- health-monitor-repo/src/main/resources/alfresco/extension/templates/webscripts/alfresco/bootstrap/demo-bootstrap-contents.js
+++ health-monitor-repo/src/main/resources/alfresco/extension/templates/webscripts/alfresco/bootstrap/demo-bootstrap-contents.js
@@ -13,26 +13,26 @@ var contents = [
 				"nodeCount": "13"\
 			}, {\
 				"nodeRef": "workspace://SpacesStore/5987deff-bf56-3d56-98ee-ae983fe556",\
-				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
+				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:main/cm:lesser/cm:private/cm:documents/cm:coroporate/cm:azcompany/cm:island/cm:city/cm:agent/cm:smith/cm:personal/cm:contracts/cm:new/cm:today",\
 				"nodeCount": "20"\
 			}],\
 			"depth": [{\
 				"nodeRef": "workspace://SpacesStore/6e8f1977-26fb-4d13-9558-849845deef,\
-				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
-				"nodeCount": "55"\
+				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:2016/cm:10/cm:3/cm:google/cm:contracts/cm:software/cm:agreements/cm:standart/cm:private/cm:aee57afc-ee65a/",\
+				"nodeCount": "16"\
 			}, {\
 				"nodeRef": "workspace://SpacesStore/d89e777a667e-26fb-26fb-9558-eaf5684ae1",\
-				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
-				"nodeCount": "68"\
+				"path": "/app:company_home/st:sites/cm:pictures/cm:documentLibrary/cm:italy/cm:sites/cm:nice/cm:sunny/cm:HTC_Evo/cm:2018/cm:08/cm:03/cm:buildings/cm:cathedrals/cm:netherlands/cm:usiancity",\
+				"nodeCount": "17"\
 			}, {\
 				"nodeRef": "workspace://SpacesStore/6e8f1977-26fb-4d13-9558-85ea453843",\
 				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
-				"nodeCount": "35"\
+				"nodeCount": "15"\
 			}]\
 		}\
 	}',
 	'{ \
-		"time": "2018-11-30-18-16",\
+		"time": "2018-06-03-11-30",\
 		"server": "192.168.10.15",\
 		"data": {\
 			"count": [{\
@@ -49,17 +49,49 @@ var contents = [
 				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
 				"nodeCount": "55"\
 			}, {\
-				"nodeRef": "workspace://SpacesStore/d89e777a667e-26fb-26fb-9558-d81f407b643c",\
+				"nodeRef": "workspace://SpacesStore/d89e777a667e-26fb-26fb-9558-ea28ac48654",\
 				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
 				"nodeCount": "68"\
 			}, {\
-				"nodeRef": "workspace://SpacesStore/d89e777a667e-26fb-26fb-9558-d81f407b643c",\
+				"nodeRef": "workspace://SpacesStore/d89e777a667e-26fb-26fb-9558-f846ac5c98",\
 				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
 				"nodeCount": "34"\
 			}, {\
 				"nodeRef": "workspace://SpacesStore/6e8f1977-26fb-4d13-9558-d81f407b643c",\
-				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
+				"path": "/app:company_home/st:sites/cm:pictures/cm:documentLibrary/cm:italy/cm:sites/cm:nice/cm:sunny/cm:HTC_Evo/cm:2018/cm:08/cm:03/cm:buildings/cm:cathedrals",\
 				"nodeCount": "11"\
+			}]\
+		}\
+	}',
+	'{ \
+		"time": "2017-01-10-18-35",\
+		"server": "host",\
+		"data": {\
+			"count": [{\
+				"nodeRef": "workspace://SpacesStore/e594a35c-26fb-4d13-9558-ea984faa3",\
+				"path": "/app:company_home/st:sites/cm:hobbySite/cm:documentLibrary/cm:music/cm:genre/cm:rock/cm:90s/cm:radiohead/cm:high/",\
+				"nodeCount": "15"\
+			}, {\
+				"nodeRef": "workspace://SpacesStore/6e8f1977-26fb-4d13-26fb-da4568d235af6",\
+				"path": "/app:company_home/st:sites/cm:pictures/cm:documentLibrary/cm:italy/cm:sites/cm:nice/cm:sunny/cm:HTC_Evo/cm:2018/cm:08/cm:03/cm:buildings/cm:cathedrals",\
+				"nodeCount": "13"\
+			}, {\
+				"nodeRef": "workspace://SpacesStore/5987deff-bf56-3d56-98ee-ae983fe556",\
+				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:main/cm:lesser/cm:private/cm:documents/cm:coroporate/cm:azcompany/cm:island/cm:city/cm:agent/cm:smith/cm:personal/cm:contracts/cm:new/cm:today",\
+				"nodeCount": "20"\
+			}],\
+			"depth": [{\
+				"nodeRef": "workspace://SpacesStore/6e8f1977-26fb-4d13-9558-849845deef,\
+				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
+				"nodeCount": "55"\
+			}, {\
+				"nodeRef": "workspace://SpacesStore/d89e777a667e-26fb-26fb-9558-eaf5684ae1",\
+				"path": "/app:company_home/st:sites/cm:site-2/cm:documentLibrary/cm:FolderLevel1/cm:FolderLevel2/cm:FolderLevel3/cm:FolderLevel4/cm:FolderLevel5/cm:FolderLevel6/cm:FolderLevel7/cm:FolderLevel8/cm:FolderLevel9/cm:FolderLevel10/cm:FolderLevel11/cm:FolderLevel12/cm:FolderLevel13/cm:FolderLevel14/cm:FolderLevel15/cm:FolderLevel16/cm:FolderLevel17/cm:FolderLevel18/cm:FolderLevel19",\
+				"nodeCount": "68"\
+			}, {\
+				"nodeRef": "workspace://SpacesStore/6e8f1977-26fb-4d13-9558-85ea453843",\
+				"path": "/app:company_home/st:sites/cm:my_site/cm:documentLibrary/cm:documents/cm:textfiles/cm:darpa/cm:top_sercret/cm:2018_5_3/cm:final/",\
+				"nodeCount": "35"\
 			}]\
 		}\
 	}'
