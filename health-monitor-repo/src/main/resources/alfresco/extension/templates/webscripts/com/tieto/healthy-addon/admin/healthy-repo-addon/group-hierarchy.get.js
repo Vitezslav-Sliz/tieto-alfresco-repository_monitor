3e84ffae7c8ea7f7016a9cad9835f1d8fe2b5f76
Vitezslav Sliz <vitezslav.sliz@tieto.com>
2018-01-16 12:46:59 +0100

    Remove dummy data for groups hierarchy

--- health-monitor-repo/src/main/resources/alfresco/extension/templates/webscripts/com/tieto/healthy-addon/admin/healthy-repo-addon/group-hierarchy.get.js
+++ health-monitor-repo/src/main/resources/alfresco/extension/templates/webscripts/com/tieto/healthy-addon/admin/healthy-repo-addon/group-hierarchy.get.js
@@ -1,7 +1,4 @@
 <import resource="classpath:alfresco/templates/webscripts/org/alfresco/repository/admin/admin-common.lib.js">
-<import resource="classpath:alfresco/extension/templates/webscripts/com/tieto/healthy-addon/admin/healthy-repo-addon/group-hierarchy.lib.js">
-
-dummyMethod();
 
 model.tools = Admin.getConsoleTools("group-hierarchy");
 model.metadata = Admin.getServerMetaData();
